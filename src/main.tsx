8b1668ca6458c9f1ef69c02b71e7869911687c9a
Saray <sryelzn@gmail.com>
2024-02-13 11:02:53 -0500

    Start game-hub-proj

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './learning-proj/components/App'
+//import App from './expense-tracker-proj/components/App/App'
+//import App from './fetching-data-proj/components/App'
+import App from './game-hub-proj/components/App'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+
+    <App />
+
+  </React.StrictMode>,
+)
