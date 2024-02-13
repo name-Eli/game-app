9fc018578bfcba6af033842eeb484078e43fc7be
Saray <sryelzn@gmail.com>
2024-02-13 12:35:31 -0500

    Implement dark mode and update logo

--- src/game-hub-proj/components/NavBar.tsx
+++ src/game-hub-proj/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpeg';
 
 const NavBar = () => {
     return (
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import ReactDOM from 'react-dom/client'
 //import App from './fetching-data-proj/components/App'
 import App from './game-hub-proj/components/App'
 import 'bootstrap/dist/css/bootstrap.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './game-hub-proj/theme'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
