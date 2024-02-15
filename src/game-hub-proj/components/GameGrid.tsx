c4e7d9a5113d6f575f9a41650459a71fa8eb1a5a
Saray <sryelzn@gmail.com>
2024-02-15 14:27:28 -0500

    Clean GameGrid

--- src/game-hub-proj/components/GameGrid.tsx
+++ src/game-hub-proj/components/GameGrid.tsx
@@ -13,8 +13,9 @@ const GameGrid = ({ gameQueryBy }: Props) => {
     const { games, error, isLoading } = useGames(gameQueryBy);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
-    return (<>
-        {error && <Text>{error}</Text>}
+    if (error) return <Text>{error}</Text>;
+
+    return (
         <SimpleGrid
             columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
             padding='10px'
@@ -28,7 +29,6 @@ const GameGrid = ({ gameQueryBy }: Props) => {
                         <GameCard key={game.id} game={game} />)
             }
         </SimpleGrid>
-    </>
     );
 }
 
