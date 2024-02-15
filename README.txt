4f8b3f4d8ba2c78a589950fee65c83d57db614db
Saray <sryelzn@gmail.com>
2024-02-15 18:35:16 -0500

    Order files

---
+++ README.txt
@@ -0,0 +1,6 @@
+Hi, if this is your first time downloading this repository, please follow these steps in the command line:
+
+1) Run 'npm install'
+2) Run 'npm run dev'
+
+Enjoy :]
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './learning-proj/components/App'
-//import App from './expense-tracker-proj/components/App/App'
-//import App from './fetching-data-proj/components/App'
-import App from './game-hub-proj/components/App'
+import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.css'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from './game-hub-proj/theme'
+import theme from './theme'
 import './index.css'
 
 
