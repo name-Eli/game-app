7aecf9bd87e34800ccecccc2abcfc02f2f7d3de5
Saray <sryelzn@gmail.com>
2024-03-11 20:12:48 -0400

    Cleaned up 'useFetchData' hook

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,43 +1,43 @@
 import { useEffect, useState } from "react";
-import api from '../services/api'
+import api from '../services/api';
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-
 interface IFetchResponse<T> {
-    count: number;
     results: T[];
 }
 
-const useFetchData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, effectDependencies?: any[]) => {
-    const [data, setData] = useState<T[]>();
-    const [error, setError] = useState();
-    const [isLoading, setIsLoading] = useState(true);
+const useFetchData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, effectDependencies: any[] = []) => {
+    const [data, setData] = useState<T[] | undefined>();
+    const [error, setError] = useState<string | undefined>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        getData();
-    }, effectDependencies ? [...effectDependencies] : []);
-
-    async function getData() {
-        setIsLoading(true);
         const controller = new AbortController();
-        try {
-            const promise = await api.get<IFetchResponse<T>>(endpoint, {
-                signal: controller.signal,
-                ...requestConfig
-            });
-            setData(promise.data.results);
-        }
-        catch (err: any) {
-            if (err instanceof CanceledError) return;
-            setError(err.message);
-        }
-        finally {
-            setIsLoading(false);
-            controller.abort();
-        }
-    }
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await api.get<IFetchResponse<T>>(endpoint, {
+                    signal: controller.signal,
+                    ...requestConfig
+                });
+                setData(response.data.results);
+            }
+            catch (err: any) {
+                if (err instanceof CanceledError) return;
+                setError(err.message);
+            }
+            finally {
+                setIsLoading(false);
+                controller.abort();
+            }
+        };
+
+        fetchData();
+
+        return () => controller.abort();
+    }, effectDependencies);
 
     return { data, error, isLoading };
-}
+};
 
 export default useFetchData;
