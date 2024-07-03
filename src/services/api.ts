81670f71596182ef7065d2dbd846175125648025
Saray <sryelzn@gmail.com>
2024-07-03 17:18:08 -0400

    New api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: '8f4600bc2a5c46dc932f5bc387ceb501'
+        key: 'c599398a2bc44a8b8064a8f783b60618'
     }
 });
 
